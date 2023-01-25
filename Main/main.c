f244c525135462fe9bcaef42389c74df68cfe37b
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2023-01-25 01:00:07 +0000

    main code

---
+++ Main/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	char *s = "HELLO RHODA";
+	int len1;
+	int len2;
+
+	printf("%");
+	_printf("%");
+	_printf("\n");
+	/* Test format string is NULL - no output expected */
+	len1 = printf(NULL);
+	len2 = _printf(NULL);
+	printf("std return: %d, _printf return: %d\n", len1, len2);
+
+	/* Test parameter string is NULL - expect print (null) */
+	len1 = printf("Null is %s\n", NULL);
+	len2 = _printf("Null is %s\n", NULL);
+	printf("std return: %d, _printf return: %d\n", len1, len2);
+
+	/* Test with empty string */
+	len1 = printf("Nothing should be after this statement: %s\n", "");
+	len2 = _printf("Nothing should be after this statement: %s\n", "");
+	printf("std return: %d, _printf return: %d\n", len1, len2);
+
+	/* Test if input is a string directly entered */
+	len1 = printf("Test string: %s\n", "YES!");
+	len2 = _printf("Test string: %s\n", "YES!");
+	printf("std return: %d, _printf return: %d\n", len1, len2);
+
+	/* Test input of a string variable */
+	len1 = printf("Test string variable: %s\n", s);
+	len2 = _printf("Test string variable: %s\n", s);
+	printf("std return: %d, _printf return: %d\n", len1, len2);
+
+	/* Test input of string variable in middle of a string */
+	len1 = printf("Test %s string\n", s);
+	len2 = _printf("Test %s string\n", s);
+	printf("std return: %d, _printf return: %d\n", len1, len2);
+
+	/* Test input of special characters */
+	len1 = printf("%s Test\\ string\n", s);
+	len2 = _printf("%s Test\\ string\n", s);
+	printf("std return: %d, _printf return: %d\n", len1, len2);
+
+	/* Test %c */
+	len1 = printf("%cello\n", 'H');
+	len2 = _printf("%cello\n", 'H');
+	printf("std return: %d, _printf return: %d\n", len1, len2);
+
+	/* Test %% */
+	len1 = printf("print %%%correctly?\n", 'H');
+	len2 = _printf("print %%%correctly?\n", 'H');
+	printf("std return: %d, _printf return: %d\n", len1, len2);
+
+	/* Test %% */
+	len1 = printf("Diferent parameter %Jorrectly?\n", 'H');
+	len2 = _printf("Diferent parameter %Jorrectly?\n", 'H');
+	printf("std return: %d, _printf return: %d\n", len1, len2);
+
+	/* Test %% */
+	len1 = printf("print %       s?\n", s);
+	len2 = _printf("print %       s?\n", s);
+	printf("std return: %d, _printf return: %d\n", len1, len2);
+
+	return (0);
+}
